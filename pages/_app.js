2057e488c51a4e9c27bee19c72415e0b4ddd2c0d
Jeff Erson <74770087+Jeff-Erson44@users.noreply.github.com>
2023-04-12 00:59:12 +0200

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
+import '../styles/reset.css'
+import Layout from '../components/Layout'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+function MyApp({ Component, pageProps }) {
+  return(
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  )
 }
+
+export default MyApp
