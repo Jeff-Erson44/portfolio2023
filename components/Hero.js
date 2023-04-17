c350114a14868fdabb7088c6f17687c811f73901
Jeff Erson <74770087+Jeff-Erson44@users.noreply.github.com>
2023-04-17 20:17:46 +0200

    [EVOL] Hero desktop + responsive

--- components/Hero.js
+++ components/Hero.js
@@ -18,13 +18,43 @@ const HeroStyle = styled.section`
     h3{
         font-size: 3.75rem;
         color: var(--secondary-color);
-        margin-bottom: 175px;
+        margin-bottom: 200px;
     }
     .hero{
         max-width: 100%;
+        margin-bottom: 250px;
         &__intro{
-            background: red;
             width: 50%;
+            margin-left: 50%;
+            transform: translateX(-25%);
+            h2{
+                position: absolute;
+                z-index: -1;
+                font-size: 12rem;
+            }
+            h4{
+                font-size: 4rem;
+                margin-bottom: 8px;
+            }
+            h5{
+                margin-bottom: 45px;
+                font-size: 1.25rem;
+                font-weight: 400;
+                color: var(--secondary-color);
+            }
+            p{
+                padding: 0 200px;
+                margin-bottom: 35px;
+                margin-left: -200px;
+                line-height: 28px;
+            }
+            .button__seeMore{
+                display: flex;
+                gap: 8px;
+                a{
+                    font-weight: 600;
+                }
+            }
         }
     }
     .scroll{
@@ -44,7 +74,7 @@ const HeroStyle = styled.section`
     }
     h3{
         font-size: 1.5rem;
-        margin-bottom: 100px;
+        margin-bottom: 125px;
     }
     h1,h2,h3{
         line-height: 40px;
@@ -54,6 +84,31 @@ const HeroStyle = styled.section`
         display: block;
         text-align: center;
         font-weight: 500;
+    }
+    .hero{
+        margin-bottom: 150px;
+        &__intro{
+            margin: 0 15px;
+            width: 100%;
+            transform: translate(0);
+            h4{
+                font-size: 1.25rem;
+            }
+            h5{
+                font-size: .75rem;
+            }
+            p{
+                padding: 0 30px 0 0;
+                margin: 0 0 35px 0;
+                font-size: .8rem;
+                text-align: left;
+                line-height: 20px;
+            }
+        }
+    }
+    .scroll{
+        display: block;
+        margin-bottom: 150px;
     }
 }
 `
@@ -68,15 +123,16 @@ export default function Hero(){
                 <h3>Développeur basé à Paris</h3>
                 <p className="scroll">Scroll</p>
                 <div className="hero__intro">
-                    <h4>Jefferon Kouao</h4>
+                    <h4>Jefferson Kouao</h4>
+                    <h5>Jefferson Kouao</h5>
                     <p>Je m&#39;appelle Jefferson, jeune développeur de 23 ans basé à Paris. Avec plus de 2 ans d&#39;expérience en tant que freelance, je suis passionné par le développement web et les technologies innovantes telles que la blockchain et les cryptomonnaies.</p>
                     <div className="button__seeMore">
-                        <a>Voir plus</a>
+                        <a href={'/apropos'}>Voir plus</a>
                         <Image
                             src={'/images/icone/Arrow-diag.svg'}
                             alt='fleche'
-                            width={15}
-                            height={15}
+                            width={10}
+                            height={10}
                         />
                     </div>
                 </div>
